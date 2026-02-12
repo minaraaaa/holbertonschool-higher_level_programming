dbe5a018ff9c3f8a45fa5a6a1494e6b75746d315
minaraaaa <minaraa.ismayilova@gmail.com>
2026-02-12 21:23:07 +0000

    Fix: Added missing underscores to __name__ and corrected indentation

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-if name == "main":
-argv = sys.argv[1:]
-count = len(argv)
-if count == 0:
-print("0 arguments.")
-elif count == 1:
-print("1 argument:")
-print("1: {:s}".format(argv[0]))
-else:
-print("{:d} arguments:".format(count))
-for i, arg in enumerate(argv, 1):
-print("{:d}: {:s}".format(i, arg))
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    count = len(argv)
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+        print("1: {:s}".format(argv[0]))
+    else:
+        print("{:d} arguments:".format(count))
+        for i, arg in enumerate(argv, 1):
+            print("{:d}: {:s}".format(i, arg))

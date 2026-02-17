7632d548c8f7b69720000572d87de5e2ed12dc4d
minaraaaa <minaraa.ismayilova@gmail.com>
2026-02-17 08:12:10 +0000

    Fix pycodestyle W293: removed whitespace from blank lines in 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,6 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    
     max_key = list(a_dictionary.keys())[0]
     for key in a_dictionary:
         if a_dictionary[key] > a_dictionary[max_key]:

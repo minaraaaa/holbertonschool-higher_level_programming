f763745eaed364508ca601955208d0002bbe73cd
minaraaaa <minaraa.ismayilova@gmail.com>
2026-02-27 19:16:08 +0000

    3. String representation

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class with private attributes,
+methods to calculate area and perimeter, and a string representation.
+"""
+
+
+class Rectangle:
+    """Representation of a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initializes the rectangle."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for the private instance attribute __width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the private instance attribute __width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the private instance attribute __height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the private instance attribute __height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the rectangle area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the rectangle perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Returns the printable string representation of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rect_str = []
+        for i in range(self.__height):
+            rect_str.append("#" * self.__width)
+        return "\n".join(rect_str)

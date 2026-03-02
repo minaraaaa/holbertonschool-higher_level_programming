f954a42426c576dd386db2a28d845cd74c452233
minaraaaa <minaraa.ismayilova@gmail.com>
2026-03-02 19:58:04 +0000

    Fix PEP8 E501 in 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,21 +3,16 @@
 Adds all arguments to a Python list, and then save them to a file.
 """
 import sys
-import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-# Əgər fayl varsa, içini oxuyuruq, yoxdursa boş siyahı yaradırıq
 try:
     items = load_from_json_file(filename)
 except (FileNotFoundError, ValueError):
     items = []
 
-# Terminaldan gələn arqumentləri (skriptin adından sonrakıları) siyahıya əlavə edirik
 items.extend(sys.argv[1:])
-
-# Siyahını yenidən fayla yazırıq
 save_to_json_file(items, filename)
